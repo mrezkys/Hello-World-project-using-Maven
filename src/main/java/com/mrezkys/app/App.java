23777b28ee41555b97776f217d87b95bd2653d2b
mrezkys <mrezkysulihin@gmail.com>
2019-12-03 21:03:19 +0800

    second commit

---
+++ src/main/java/com/mrezkys/app/App.java
@@ -0,0 +1,13 @@
+package com.mrezkys.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! My name is Muhammad Rezky Sulihin" );
+    }
+}
